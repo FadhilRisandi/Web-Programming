5412e538f9d2d436d6f82d4e9d2ac82a06143e92
unknown <afadhilrk01@gmail.com>
2021-09-30 15:51:53 +0800

    upload new files

--- 04-Javascript/shopList.js
+++ 04-Javascript/shopList.js
@@ -8,7 +8,8 @@ const database = new Map();
 
 
 btnAddItem.addEventListener('click', (event) =>  {
-    const item = inputItem.value
+    const ITEM_KEY = inputItem.value
+    const ITEM_VALUE = inputItem.value;
 
     
     const listItem = document.createElement('li');
@@ -16,39 +17,39 @@ btnAddItem.addEventListener('click', (event) =>  {
     const btnDelete = document.createElement('button');
     const counter = document.createElement('button');
     
-    var count = 1
-    counter.textContent = count;
 
-    if (item === '') {
+    if (ITEM_VALUE === '') {
         alert("Item Name can't be blank");
         inputItem.focus();
         return;
       }
     
-      // WARN: Check for duplication
-      if (database.has(item)) {
-        const itemCounter = document.getElementById(counter);
-        count = Number(itemCounter);
-        itemCounter.textContent = (count + 1);
-        console.log(itemCounter)
+      // If user input the same item, display a counter showing how many related item do we have
+      // If there are only items, do not display the counter
+      if (database.has(ITEM_KEY)) {
+        const itemCounter = document.getElementById(ITEM_KEY);
+        count = Number(itemCounter.textContent);
+        itemCounter.textContent = (count+ 1);
         inputItem.value ='';
         inputItem.focus();
         return;
-       
       }
-      
+
       // NOTE: Add the new item to database
-      database.set(String(item), item);
+      database.set(ITEM_KEY, ITEM_VALUE);
     
       // NOTE: Add attribute
       listItem.classList.add('list-item'); // NOTE: Add Class
     
       // NOTE: Add value
-      textItem.textContent = item;
+      textItem.textContent = ITEM_VALUE;
       btnDelete.textContent = 'Delete';
       
       // NOTE: The counter should be dynamicly show how much do we have for this item
-     
+      var count = 1
+      counter.textContent = count;
+   
+      counter.setAttribute("id",ITEM_KEY);
      
     
       // NOTE: Combine elements
@@ -56,10 +57,10 @@ btnAddItem.addEventListener('click', (event) =>  {
       listContainer.appendChild(listItem);
       
   
-      
-  
       // NOTE: Handle click event for delete button
-      
+      // Add delete confirmation
+      // If user confirms yes, delete the item if it is the only item, otherwise decrease the counter
+      // If user confirm no/decline/press ESC, do not delete the item
       btnDelete.addEventListener('click', () => {
         const Yes = confirm('Are you sure want to delete this item')
           if (Yes){
@@ -70,12 +71,9 @@ btnAddItem.addEventListener('click', (event) =>  {
             }
           } else {
             return;
-          }
-          
-        
+          }   
       });
       
-
       inputItem.value = '';
       inputItem.focus();
     });
